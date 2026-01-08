696107920b3b80558ac815b93aca8aa3fdc0885c
Triumph <triumphaidenojie08@gmail.com>
2026-01-08 12:39:14 +0100

    style(landing): add noisytexture bg and 2 balls

--- src/app/welcome/page-content.tsx
+++ src/app/welcome/page-content.tsx
@@ -8,7 +8,9 @@ import { ReactLenis } from "lenis/react";
 export default function PageContent() {
   // useGSAP(() => {}, []);
 
-  const lenisRef = useRef();
+  const lenisRef = useRef(null);
+  const ballARef = useRef<HTMLSpanElement | null>(null);
+  const ballBRef = useRef<HTMLSpanElement | null>(null);
 
   useEffect(() => {
     function update(time) {
@@ -25,8 +27,8 @@ export default function PageContent() {
       <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
 
       <section className="flex-center relative min-h-screen w-full max-w-screen overflow-clip">
-        <span className="bg-foreground/70 absolute top-1/2 left-6/10 z-1 size-125 -translate-1/2 rounded-full blur-2xl"></span>
-        <span className="bg-chart-2/80 absolute top-14/20 left-83/100 z-1 size-125 -translate-1/2 rounded-full blur-2xl"></span>
+        <span className="bg-foreground/70 absolute top-1/2 left-6/10 z-1 size-125 -translate-1/2 rounded-full blur-2xl duration-200"></span>
+        <span className="bg-chart-2/80 absolute top-14/20 left-83/100 z-1 size-125 -translate-1/2 rounded-full blur-2xl duration-200"></span>
 
         <div className="bg-background/60 flex-center z-10 h-screen w-full bg-[url('/images/noise.png')] bg-blend-overlay backdrop-blur-3xl">
           <div className="">
