e46aaceee5939fecb23fc818c40371f31b40051d
Triumph <triumphaidenojie08@gmail.com>
2026-02-23 12:42:43 +0100

    refactor(folders): add loading screen between pages

---
+++ src/app/folders/loading.tsx
@@ -0,0 +1,16 @@
+import { QuillWrite01FreeIcons } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+
+export default function Loading() {
+  return (
+    <div className="flex-center fixed top-1/2 left-1/2 -translate-1/2">
+      <HugeiconsIcon
+        icon={QuillWrite01FreeIcons}
+        size={60}
+        color="currentColor"
+        strokeWidth={1.8}
+        className="animate-pulse"
+      />
+    </div>
+  );
+}
